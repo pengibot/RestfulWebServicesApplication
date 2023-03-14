d56735000c57c6985098e351bf31d4d9427bb46d
We Are Anonymous <pengibot@gmail.com>
2023-03-14 21:44:22 +0000

    Added Basic Auth check

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.in28minutes.rest.webservices.restfulwebservices.basic;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+
+    // Filter Chain
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.cors().and().csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers().permitAll().anyRequest().authenticated().and()
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf().disable()
+                .build();
+    }
+}
